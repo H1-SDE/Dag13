93649bd4bb9b9415a09d8e1dd006783a5c6a902d
Nima <nimabadiei@gmail.com>
2023-01-25 12:16:48 +0100

    Ezé

---
+++ dag13_opgave3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dag13_opgave4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoBookTest album = new PhotoBookTest();
+            Console.ReadLine();
+        }
+    }
+}
